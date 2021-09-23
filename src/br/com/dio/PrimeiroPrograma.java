ccfb0b38c3bd576cf474f2d603f704076367c825
Araujo <ronyaraujo11@yahoo.com.br>
2021-09-23 16:11:12 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Gato gato = new Gato(); 
+
+
+System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+       System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
